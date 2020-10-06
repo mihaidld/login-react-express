8d14e98e5e51cab7d750ff8545a3277923dd44f3
PeHaa <info@pehaa.com>
2020-10-06 11:04:44 +0200

    final

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ const __dirname = path.dirname(__filename)
 
 //our user mini-database
 const db_user = {
-    alice: '123',
-    bob: '456',
-    charlie: `789`,
+  alice: '123',
+  bob: '456',
+  charlie: `789`,
 }
 
 //middleware for checking if user exists: if inside database their is an own
@@ -24,31 +24,35 @@ const db_user = {
 //hasOwnProperty() method similar to username in db_user (which returns
 //enumerable properties also inherited)
 const userChecker = (req, res, next) => {
-    const username = req.body.username
-    console.log(req.body)
-    if (db_user.hasOwnProperty(username)) {
-        next()
-    } else {
-        res.send('Username or password invalid')
-    }
+  const username = req.body.username
+  console.log(req.body)
+  if (db_user.hasOwnProperty(username)) {
+    next()
+  } else {
+    res.send({ valid: false })
+  }
 }
 
 //middleware for checking if password is correct
 const passwordChecker = (req, res, next) => {
-    const username = req.body.username
-    const password = req.body.password
-    if (db_user[username] === password) {
-        next()
-    } else {
-        res.send('Username or password invalid')
-    }
+  const username = req.body.username
+  const password = req.body.password
+  if (db_user[username] === password) {
+    next()
+  } else {
+    res.send({ valid: false })
+  }
 }
 
-const IP = '172.18.241.127'
+const IP = '192.168.1.70'
 const PORT = 7777
 
 const app = express()
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 //Configure express to use body-parser as middleware on all routes
 //to support URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -64,10 +68,10 @@ app.use('/login', passwordChecker)
 /*We are waiting for a POST request with a body containing json data
 {'username':'alice', 'password':'123'} */
 app.post('/login', (req, res) => {
-    let username = req.body.username
-    res.send(`Welcome to your dashboard ${username}`)
+  let username = req.body.username
+  res.send({ valid: true, username })
 })
 
 app.listen(PORT, IP, () => {
-    console.log(`listening on ${IP}:${PORT}`)
+  console.log(`listening on ${IP}:${PORT}`)
 })
